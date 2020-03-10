66adbdfe4fbf8740f0d6867f705a9f225c2a0563
tien.nguyen <tien.nguyen@VNDEV21.augen.co.nz>
2020-03-10 16:10:07 +0700

    Abstract Page

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,262 @@
 package commons;
 
-public class AbstractPage {
+import java.util.List;
+import java.util.Set;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractPage {
+	
+	By by;
+	Select select;
+	Actions action; //user interactions - actions
+	WebElement element;
+	long shortTimeout = 5;
+	long longTimeout = 30;
+	WebDriver driverGlobal;
+	List<WebElement> elements;
+	WebDriverWait waitExplicit;
+	JavascriptExecutor jsExecutor;
+	
+	public AbstractPage(WebDriver driverLocal) {
+		driverGlobal = driverLocal;
+		jsExecutor = (JavascriptExecutor) driverGlobal;
+		waitExplicit = new WebDriverWait(driverGlobal,longTimeout);
+		action = new Actions(driverGlobal);	//tham so la driver, ta truyen o day
+	}
+
+	public void openUrl(String urlValue){
+		driverGlobal.get(urlValue);		
+	}
+	
+	public String getPageTitle () {
+		return driverGlobal.getTitle();
+	}
+	
+	public String getPageCurrentUrl() {
+		return driverGlobal.getCurrentUrl();
+	}
+	
+	public String getPageSource () {
+		return driverGlobal.getPageSource();
+	}
+	
+	public void backToPage() {
+		driverGlobal.navigate().back();
+	}
+	
+	public void refreshPage() {
+		driverGlobal.navigate().refresh();
+	}
+	
+	public void forwardToPage() {
+		driverGlobal.navigate().forward();
+	}
+	
+	public void acceptAlert() {
+		driverGlobal.switchTo().alert().accept();
+	}
+	
+	public void cancelAlert() {
+		driverGlobal.switchTo().alert().dismiss();
+	}
+	
+	public String getTextAlert() {
+		return driverGlobal.switchTo().alert().getText();
+	}
+	
+	public void sendKeyAlert(String value) {
+		driverGlobal.switchTo().alert().sendKeys(value);
+	}
+	
+	public void waitAlertPresence() {
+		waitExplicit.until(ExpectedConditions.alertIsPresent());
+	}
+	//Web Browser: open page/ getUrl/ getTitle/ Window/Alert/ Page Source/...
+	//Web Element: click/sendKey/submit/select/getText/getAttributes/Css/...
+	
+	public void clickToElement(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		element.click();
+	}
+	
+	public void sendKeytoElement(String locator, String value) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		element.clear();
+		element.sendKeys(value);
+	}
+	
+	public void selectItemInDropdown(String locator, String valueItem) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		select = new Select(element);
+		select.selectByVisibleText(valueItem);
+	}
+	
+	public String getValueItemInDropdown(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		select = new Select(element);
+		return select.getFirstSelectedOption().getText();
+	}
+	
+	public void selectItemInCustomDropdown(String parentLocator, String allItemsLocator, String expectedItem) {
+		element = driverGlobal.findElement(By.xpath(parentLocator));
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+		sleepInSecond(1);
+		jsExecutor.executeScript("arguments[0].click();", element);
+		sleepInSecond(1);
+		waitExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemsLocator)));
+		elements = driverGlobal.findElements(By.xpath(allItemsLocator));
+		for (WebElement item : elements) {
+			System.out.println(item.getText());
+			if (item.getText().equals(expectedItem)) {
+				jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
+				sleepInSecond(1);
+				item.click();
+				sleepInSecond(2);
+				break;
+			}
+		}
+	}
+	
+	public void sleepInSecond(long numberInSecond) {
+		try {			
+		Thread.sleep(numberInSecond * 1000);	
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getAttributeValue(String locator, String attributeName) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		return element.getAttribute(attributeName);
+	}
+	
+	public String getTextElement(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		return element.getText();
+	}
+
+	public int countElementNumber(String locator) {
+		elements = driverGlobal.findElements(By.xpath(locator));
+		return elements.size();
+	}
+	
+	public void checkTheCheckbox(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		if (element.isSelected()==false) {
+			element.click();
+		}
+	}
+	
+	public void uncheckTheCheckbox(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		if (element.isSelected()==true) {
+			element.click();
+		}
+	}
+	
+	public boolean isElementDisplayed(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		return element.isDisplayed();
+	}
+
+	public boolean isElementSelected(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		return element.isSelected();
+	}
+	
+	public boolean isElementEnabled(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		return element.isEnabled();
+	}
+	
+	public void switchToChildWindowByID(String parent) {
+		Set<String> allWindows = driverGlobal.getWindowHandles();
+		for (String runWindow : allWindows) {
+			if (!runWindow.equals(parent)) {
+				driverGlobal.switchTo().window(runWindow);
+				break;
+			}
+		}
+	}
+	
+	public void switchToChildWindowByTitle(String title) {
+		Set<String> allWindows = driverGlobal.getWindowHandles();
+		for (String runWindows : allWindows) {
+			driverGlobal.switchTo().window(runWindows);
+			String currentWin = driverGlobal.getTitle();
+			if (currentWin.equals(title)) {
+				break;
+			}
+		}
+	}
+	
+	public boolean closeAllWindowsWithoutParent(String parentWindow) {
+		Set<String> allWindows = driverGlobal.getWindowHandles();
+		for (String runWindows : allWindows) {
+			if (!runWindows.equals(parentWindow)) {
+				driverGlobal.switchTo().window(runWindows)
+				driverGlobal.close();
+			}
+		}
+		driverGlobal.switchTo().window(parentWindow);
+		if (driverGlobal.getWindowHandles().size()==1)
+			return true;
+		else
+			return false;
+	}
+	
+	public void switchToFrameOrIframe(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		driverGlobal.switchTo().frame(element);
+	}
+	
+	public void switchToParentPage() {
+		driverGlobal.switchTo().defaultContent();
+	}
+	
+	public void hoverToElement(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		action.moveToElement(element).perform();
+	}
+	
+	public void doubleToElement(String locator) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		action.doubleClick(element).perform();
+	}
+	
+	public void sendKeyboardToElement (String locator, Keys key) {
+		element = driverGlobal.findElement(By.xpath(locator));
+		action.sendKeys(element,key).perform();
+	}
+	
+	public void waitToElementVisible(String locator) {
+		by = By.xpath(locator);
+		waitExplicit.until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+	
+	public void waitToElementPresence(String locator) {
+		by = By.xpath(locator);
+		waitExplicit.until(ExpectedConditions.presenceOfElementLocated(by));
+	}
+	
+	public void waitToElementInvisible(String locator) {
+		by = By.xpath(locator);
+		waitExplicit.until(ExpectedConditions.invisibilityOfElementLocated(by));
+	}
+	
+	public void waitToElementClickable(String locator) {
+		by = By.xpath(locator);
+		waitExplicit.until(ExpectedConditions.elementToBeClickable(by));
+	}
+	
+	
 }
--- testcases/com/nopcommerce/payment/Level_01_Step_By_Step.java
+++ testcases/com/nopcommerce/payment/Level_01_Step_By_Step.java
@@ -19,95 +19,105 @@ public class Level_01_Step_By_Step {
 	WebDriver driver;
 	Select select;
 	String email;
-	
-  @BeforeClass
-  public void beforeClass() {
-	  driver = new FirefoxDriver();
-	  email = "john_wick_" + randomNumber() + "@gmail.com";
-	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  driver.get("https://demo.nopcommerce.com/");
-  }
-	
-  @Test
-  public void TC_01_Register() {
-	  //click to Register page
-	  driver.findElement(By.xpath("//a[@class='ico-register' and text()='Register']")).click();
-	  
-	  //Verify Register Page displayed
-	  // Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page registration-page")).isDisplayed());
-	  assertTrue(driver.findElement(By.xpath("//div[@class='page registration-page']")).isDisplayed());
-	  
-	  //Click to Gender radio button
-	  driver.findElement(By.xpath("//input[@id='gender-female']")).click();
-	  
-	  //Enter First Name
-	  driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("John");
-	  
-	  //Enter Last Name
-	  driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Wick");
-	  
-	  //Select DOB dropdown
-	  select = new Select (driver.findElement(By.xpath("//select[@name='DateOfBirthDay']")));
-	  select.selectByVisibleText("10");
-	  
-	  select = new Select (driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']")));
-	  select.selectByVisibleText("October");
-	  
-	  select = new Select (driver.findElement(By.xpath("//select[@name='DateOfBirthYear']")));
-	  select.selectByVisibleText("2000");
-	  
-	  //Enter email
-	  
-	  driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
-	  
-	  //Enter Company Name
-	  driver.findElement(By.xpath("//input[@id='Company']")).sendKeys("Augen");
-	  
-	  //Enter Password and Confirm Password
-	  driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
-	  driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123456");
-	  
-	  //Click Register
-	  driver.findElement(By.xpath("//input[@id='register-button']")).click();
-	  
-	  //Verify Register successfully
-	  assertTrue(driver.findElement(By.xpath("//div[@class='result'and text() = 'Your registration completed']")).isDisplayed());
-	  
-	  //Click to Logout page
-	  driver.findElement(By.xpath("//a[@class='ico-logout' and text() = 'Log out']")).click();
-	  
-	  //Verify navigate to Home page successfully
-	  //Assert.assertEquals(driver.getCurrentUrl(), "https://demo.nopcommerce.com/");
-	  assertEquals(driver.getCurrentUrl(), "https://demo.nopcommerce.com/");
-  }
- 
-  @Test
-  public void TC_02_Login() {
-	  driver.findElement(By.xpath("//a[@class='ico-login' and text()='Log in']")).click();
-	  //Verify Login page displayed
-	  assertTrue(driver.findElement(By.xpath("//div[@class='page login-page']")).isDisplayed());
-	  
-	  //Enter email
-	  driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
-	  
-	  //Enter Password
-	  driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
-	  
-	  //Click Login button
-	  driver.findElement(By.cssSelector(".login-button")).click();
-	  
-	  //Verify My Account link displayed
-	  assertTrue(driver.findElement(By.xpath("//a[@class='ico-account' and text() = 'My account']")).isDisplayed());
-	  	  
-  }
- 
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
-  
-  public int randomNumber() {
-	  Random random = new Random();
-	  return random.nextInt(999999);
-  }
+
+	@BeforeClass
+	public void beforeClass() {
+		// set for the new Firefox version
+		// System.setProperty("webdriver.gecko.driver",".\\resources\\geckodriver.exe");
+		String rootFolder = System.getProperty("user.dir");
+		// D:\Automation\03_Java_PageObject
+		System.setProperty("webdriver.gecko.driver", rootFolder + "\\resources\\geckodriver.exe");
+		// Disable logs
+		System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, rootFolder + "\\Firefoxlogs.txt");
+		driver = new FirefoxDriver();
+		email = "john_wick_" + randomNumber() + "@gmail.com";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+	}
+
+	@Test
+	public void TC_01_Register() {
+		// click to Register page
+		driver.findElement(By.xpath("//a[@class='ico-register' and text()='Register']")).click();
+
+		// Verify Register Page displayed
+		// Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page
+		// registration-page")).isDisplayed());
+		assertTrue(driver.findElement(By.xpath("//div[@class='page registration-page']")).isDisplayed());
+
+		// Click to Gender radio button
+		driver.findElement(By.xpath("//input[@id='gender-female']")).click();
+
+		// Enter First Name
+		driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys("John");
+
+		// Enter Last Name
+		driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys("Wick");
+
+		// Select DOB dropdown
+		select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']")));
+		select.selectByVisibleText("10");
+
+		select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']")));
+		select.selectByVisibleText("October");
+
+		select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthYear']")));
+		select.selectByVisibleText("2000");
+
+		// Enter email
+
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
+
+		// Enter Company Name
+		driver.findElement(By.xpath("//input[@id='Company']")).sendKeys("Augen");
+
+		// Enter Password and Confirm Password
+		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
+		driver.findElement(By.xpath("//input[@id='ConfirmPassword']")).sendKeys("123456");
+
+		// Click Register
+		driver.findElement(By.xpath("//input[@id='register-button']")).click();
+
+		// Verify Register successfully
+		assertTrue(driver.findElement(By.xpath("//div[@class='result'and text() = 'Your registration completed']"))
+				.isDisplayed());
+
+		// Click to Logout page
+		driver.findElement(By.xpath("//a[@class='ico-logout' and text() = 'Log out']")).click();
+
+		// Verify navigate to Home page successfully
+		// Assert.assertEquals(driver.getCurrentUrl(), "https://demo.nopcommerce.com/");
+		assertEquals(driver.getCurrentUrl(), "https://demo.nopcommerce.com/");
+	}
+
+	@Test
+	public void TC_02_Login() {
+		driver.findElement(By.xpath("//a[@class='ico-login' and text()='Log in']")).click();
+		// Verify Login page displayed
+		assertTrue(driver.findElement(By.xpath("//div[@class='page login-page']")).isDisplayed());
+
+		// Enter email
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
+
+		// Enter Password
+		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("123456");
+
+		// Click Login button
+		driver.findElement(By.cssSelector(".login-button")).click();
+
+		// Verify My Account link displayed
+		assertTrue(driver.findElement(By.xpath("//a[@class='ico-account' and text() = 'My account']")).isDisplayed());
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int randomNumber() {
+		Random random = new Random();
+		return random.nextInt(999999);
+	}
 }
