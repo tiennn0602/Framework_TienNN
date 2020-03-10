e5b292a88536797f0104e591a0755f0a75fa36b5
tien.nguyen <tien.nguyen@VNDEV21.augen.co.nz>
2020-03-10 17:21:04 +0700

    Level_03_Page_Object_Pattern_NotFinished

--- actions/pageObjects/LoginPageObject.java
+++ actions/pageObjects/LoginPageObject.java
@@ -2,4 +2,19 @@ package pageObjects;
 
 public class LoginPageObject {
 
+	public void inputToEmailTextbox(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToPasswordTextbox(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void clickLoginButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- actions/pageObjects/RegisterPageObject.java
+++ actions/pageObjects/RegisterPageObject.java
@@ -2,4 +2,49 @@ package pageObjects;
 
 public class RegisterPageObject {
 
+	public void clickToMaleRadioButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToFirstnameTextbox(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToLastnameTextbox(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToEmailTextbox(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToPasswordTextbox(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void inputToConfirmPasswordTextbox(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void clickRegisterButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isSuccessfulMessageDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void clickLogoutLink() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
